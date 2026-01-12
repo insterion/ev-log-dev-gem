4a5312dde24d51a3aa01a529e1d48125276e72bb
insterion <insterion@gmail.com>
2026-01-12 09:25:54 +0000

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,41 +1,34 @@
-/* calc.js - Mathematical logic */
+/* calc.js - Calculation Logic */
+
 const Calc = {
-    // Конвертиране
-    litersToGallons: (l) => l / 4.54609,
-    gallonsToLiters: (g) => g * 4.54609,
-
     /**
-     * Сравнява EV и ICE за дадено количество ток
-     * @param {number} kwh - Зареден ток
-     * @param {number} evPrice - Цена на тока (£/kWh)
-     * @param {number} evEff - Ефективност (mi/kWh)
-     * @param {number} iceMpg - Разход на старата кола (MPG)
-     * @param {number} fuelPriceLiter - Цена на горивото (£/L)
+     * Сравнява зареждане на EV с еквивалентен пробег на ICE (ДВГ)
+     * @param {number} kwh - Заредени киловатчаса
+     * @param {number} pricePerKwh - Цена на тока
+     * @param {number} evEfficiency - Ефективност (мил/kWh)
+     * @param {number} iceMpg - Разход на ДВГ (Miles Per Gallon)
+     * @param {number} fuelPricePerLiter - Цена на горивото
      */
-    compare: function(kwh, evPrice, evEff, iceMpg, fuelPriceLiter) {
-        // 1. Колко мили ще минем с този ток?
-        const range = kwh * evEff;
+    compare: function(kwh, pricePerKwh, evEfficiency, iceMpg, fuelPricePerLiter) {
+        // 1. Колко мили минаваме с този ток?
+        const rangeMiles = kwh * evEfficiency;
 
-        // 2. Колко струва това с EV?
-        const costEV = kwh * evPrice;
+        // 2. Колко струва това на ток?
+        const costEV = kwh * pricePerKwh;
 
-        // 3. Колко гориво (литри) ни трябват за същите мили?
-        // Gallons needed = Miles / MPG
-        const gallons = iceMpg > 0 ? range / iceMpg : 0;
-        const liters = gallons * 4.54609;
-
-        // 4. Колко струва това с ДВГ?
-        const costICE = liters * fuelPriceLiter;
-
-        // 5. Разлика
-        const diff = costICE - costEV;
+        // 3. Колко би струвало с ДВГ?
+        // UK Gallon = 4.54609 литра
+        const gallonsNeeded = rangeMiles / iceMpg;
+        const litersNeeded = gallonsNeeded * 4.54609;
+        
+        const costICE = litersNeeded * fuelPricePerLiter;
 
         return {
-            rangeMiles: range,
+            rangeMiles: rangeMiles,
             costEV: costEV,
             costICE: costICE,
-            savings: diff,
-            isCheaper: diff > 0
+            savings: costICE - costEV, // Положително число = спестено
+            isCheaper: costEV < costICE
         };
     }
 };
